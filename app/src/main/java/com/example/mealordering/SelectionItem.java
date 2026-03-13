96fe0b7576ac40d6ce026a9a7e3c656851266854
Myke <dejesusmykelouies@gmail.com>
2026-03-13 11:22:34 +0800

    9

--- app/src/main/java/com/example/mealordering/SelectionItem.java
+++ app/src/main/java/com/example/mealordering/SelectionItem.java
@@ -1,6 +1,13 @@
 package com.example.mealordering;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class SelectionItem extends AppCompatActivity {
+
+    TextView tvMainDish;
+
+    CheckBox cbNone, cbFries, cbLumpia, cbSoup, cbRice;
+
+    int numOfSide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,89 @@ public class SelectionItem extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        numOfSide = 0;
+
+        tvMainDish = findViewById(R.id.tvMainDish);
+        tvMainDish.setText(DataHolder.selectedMainDishName);
+
+        cbNone = findViewById(R.id.cbNone);
+        cbFries = findViewById(R.id.cbFries);
+        cbLumpia = findViewById(R.id.cbLumpia);
+        cbSoup = findViewById(R.id.cbSoup);
+        cbRice = findViewById(R.id.cbRice);
+
+//        TimePickerDialog tpd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+//            @Override
+//            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//
+//            }
+//        }, 1, 1, false);
+//        tpd.show();
     }
+
+    public void cbNonePressed(View view) {
+
+        if(cbNone.isChecked()) {
+
+            cbFries.setChecked(false);
+            cbFries.setEnabled(false);
+
+            cbLumpia.setChecked(false);
+            cbLumpia.setEnabled(false);
+
+            cbSoup.setChecked(false);
+            cbSoup.setEnabled(false);
+
+            cbRice.setChecked(false);
+            cbRice.setEnabled(false);
+
+        } else {
+
+            cbFries.setEnabled(true);
+            cbLumpia.setEnabled(true);
+            cbSoup.setEnabled(true);
+            cbRice.setEnabled(true);
+            numOfSide = 0;
+
+        }
+    }
+
+    public void sidePressed(View view) {
+
+        if(((CheckBox)view).isChecked()) {
+            numOfSide++;
+        } else {
+            numOfSide--;
+        }
+
+        if(numOfSide == 2) {
+
+            if(!cbFries.isChecked()){
+                cbFries.setEnabled(false);
+            }
+            if(!cbRice.isChecked()){
+                cbRice.setEnabled(false);
+            }
+            if(!cbSoup.isChecked()){
+                cbSoup.setEnabled(false);
+            }
+            if(!cbLumpia.isChecked()){
+                cbLumpia.setEnabled(false);
+            }
+
+        } else {
+
+            cbFries.setEnabled(true);
+            cbLumpia.setEnabled(true);
+            cbSoup.setEnabled(true);
+            cbRice.setEnabled(true);
+
+        }
+    }
+
+    public void backPressed(View view) {
+        this.finish();
+    }
+
 }
