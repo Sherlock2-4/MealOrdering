3cc24f54b9ddc0685f12dc7d4d641270a464deac
Myke <dejesusmykelouies@gmail.com>
2026-03-13 10:19:57 +0800

    5

--- app/src/main/java/com/example/mealordering/MainActivity.java
+++ app/src/main/java/com/example/mealordering/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mealordering;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    MainDishAdapter adapter;
+    List<MainDishListItem> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lv = findViewById(R.id.lvMainDish);
+
+        data = new ArrayList<>();
+        data.add(new MainDishListItem(getDrawable(R.drawable.chicken_rice), "Chicken Rice", 95.00));
+        data.add(new MainDishListItem(getDrawable(R.drawable.beef_tapa), "Beef Tapa", 120.00));
+        data.add(new MainDishListItem(getDrawable(R.drawable.pork_sisig), "Pork Sisig", 130.00));
+        data.add(new MainDishListItem(getDrawable(R.drawable.veggie_bowl), "Veggie Bowl", 85.00));
+
+        adapter = new MainDishAdapter(this, data);
+        lv.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/mealordering/MainDishAdapter.java
+++ app/src/main/java/com/example/mealordering/MainDishAdapter.java
@@ -1,5 +1,77 @@
 package com.example.mealordering;
 
-public class MainDishAdapter{
-    
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainDishAdapter extends BaseAdapter {
+
+    LayoutInflater inflater;
+    List<MainDishListItem> data;
+
+    public MainDishAdapter(Context context, List<MainDishListItem> data){
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data == null ? 0 : data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    static class ViewHolder {
+
+        ImageView ivDish;
+        TextView tvDishName;
+        TextView tvDishPrice;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder vh;
+
+        if(convertView == null) {
+
+            convertView = inflater.inflate(R.layout.menu_item, parent, false);
+            vh = new ViewHolder();
+            vh.ivDish = convertView.findViewById(R.id.ivDish);
+            vh.tvDishName = convertView.findViewById(R.id.tvName);
+            vh.tvDishPrice = convertView.findViewById(R.id.tvPrice);
+
+            convertView.setTag(vh);
+
+        } else {
+
+            vh = (ViewHolder) convertView.getTag();
+
+        }
+
+        MainDishListItem item = data.get(position);
+        vh.ivDish.setImageDrawable(item.dMainDish);
+        vh.tvDishName.setText(item.mainDishName);
+        vh.tvDishPrice.setText("₱  " + item.mainDishPrice);
+
+        return convertView;
+    }
 }
